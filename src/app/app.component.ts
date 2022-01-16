cc4d9eb7473c2957295aff40ba0f4df1bd184887
smoonmare <smoonmare@gmail.com>
2022-01-16 10:06:01 -0800

    Fixed type issue for template. Declered inProgress/Done arrays. Declared edit/drop methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { Task } from './task/task';
 })
 export class AppComponent {
   title = 'giakomo-kanban';
-  todo: Task[] = [
+  todo: Task[] | null = [
     {
       title: 'Buy milk',
       description: 'Go to the store and buy milk'
@@ -18,4 +19,23 @@ export class AppComponent {
       description: 'Using Angular and Firebase create a Kanban App'
     }
   ];
+  inProgress: Task[] | null = [];
+  done: Task[] | null = [];
+
+  editTask(list: 'todo' | 'done' | 'inProgress', task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]|null>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
